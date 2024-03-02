8858815a23a8ed966cb586af37503973028a437b
Ronit Dutta <96005252+WebRonit@users.noreply.github.com>
2024-03-02 16:53:04 +0530

    Update and rename manifest.json to manifest.js

---
+++ src/app/manifest.js
@@ -0,0 +1,18 @@
+export default function manifest() {
+  return {
+    name: 'Cake Shop',
+    short_name: 'Cake Shop',
+    description: 'Order cakes online',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
